36daf23bbff20b17bab2bb292f212313944be81d
Michael Woolnough <michael.woolnough@gmail.com>
2023-06-13 07:47:15 +0100

    added Request tests

---
+++ client_test.go
@@ -0,0 +1,88 @@
+package jsonrpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"testing"
+)
+
+type rw struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+func (r *rw) Close() error {
+	r.ReadCloser.Close()
+	r.WriteCloser.Close()
+
+	return nil
+}
+
+func makeServerClientConn() (ReadWriteCloser, ReadWriteCloser) {
+	ar, bw := io.Pipe()
+	br, aw := io.Pipe()
+
+	return &rw{
+			ReadCloser:  ar,
+			WriteCloser: aw,
+		}, &rw{
+			ReadCloser:  br,
+			WriteCloser: bw,
+		}
+}
+
+type simpleHandler struct{}
+
+var ErrUnknownEndpoint = &Error{
+	Message: "unknown endpoint",
+}
+
+func (simpleHandler) HandleRPC(method string, data json.RawMessage) (any, error) {
+	switch method {
+	case "add":
+		var toAdd [2]int
+		if err := json.Unmarshal(data, &toAdd); err != nil {
+			return nil, err
+		}
+
+		return toAdd[0] + toAdd[1], nil
+	}
+	return nil, ErrUnknownEndpoint
+}
+
+func TestRequest(t *testing.T) {
+	serverConn, clientConn := makeServerClientConn()
+
+	s := New(serverConn, new(simpleHandler))
+	go s.Handle()
+	defer serverConn.Close()
+
+	c := NewClient(clientConn)
+	defer c.Close()
+
+	for n, test := range [...]struct {
+		Method   string
+		Params   any
+		Response json.RawMessage
+		Error    error
+	}{
+		{
+			Method: "unknown",
+			Error:  ErrUnknownEndpoint,
+		},
+		{
+			Method:   "add",
+			Params:   [2]int{5, 6},
+			Response: json.RawMessage{'1', '1'},
+		},
+	} {
+		resp, err := c.Request(test.Method, test.Params)
+		if !errors.Is(test.Error, err) {
+			t.Errorf("test %d: expecting error %s, got %s", n+1, test.Error, err)
+		} else if !bytes.Equal(test.Response, resp) {
+			t.Errorf("test %d: expecting response %s, got %s", n+1, test.Response, resp)
+		}
+	}
+}
